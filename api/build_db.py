f4bb9e872ea299107fecc63163150e408adfd91d
Brian Abelson <brianabelson@gmail.com>
2013-11-16 14:49:26 -0500

    removed dependency on pandas

--- api/build_db.py
+++ api/build_db.py
@@ -1,34 +1,56 @@
-import pandas as pd 
 import dataset
-import math
+import os
+import csv
 
 db_url = os.getenv('DATABASE_URL')
 db = dataset.connect(db_url)
 
-df = pd.read_csv('../cleaned_data/lotus_database.csv')
+# read in csv
+fieldnames = (
+  "document",
+  "id",
+  "subject",
+  "year",
+  "year_case_count",
+  "category",
+  "country",
+  "gender",
+  "appeal_date",
+  "issue_date",
+  "action",
+  "all_dates",
+  "body"
+)
+f = open( '../cleaned_data/lotus_database.csv', 'r' )
+reader = csv.DictReader(f, fieldnames = fieldnames)
+for row in reader:
+  print reader
 
-data = []
-for i in df.index:
-  row = {}
-  for k in df.keys():
-    v = df[k][i]
-    if pd.isnull(v):
+print raw_data[0]
+clean_data = []
+for row in raw_data:
+  new_row = {}
+  for k in row.keys():
+    v = row[k]
+    if v == '':
       v = None
     if k == "id":
-      row['data_id'] = v
+      new_row['data_id'] = v
     if k == "action":
-      row['action'] = str(v)
+      new_row['action'] = str(v)
     if k == "year":
       if v is None:
-        row['year'] = None
+        new_row['year'] = None
       else:
-        row['year'] = int(math.floor(float(v)))
+        print v
+        new_row['year'] = int(v.strip())
     else:
-      row[k] = v
-    row.pop("id", None)
+      new_row[k] = v
 
-  data.append(row)
+    new_row.pop("id", None)
+
+  clean_data.append(new_row)
 
 print "inserting data into postgres"
 db['amnesty'].delete()
-db['amnesty'].insert_many(data)
+db['amnesty'].insert_many(clean_data)
