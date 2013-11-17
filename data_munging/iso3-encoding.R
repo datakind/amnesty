1d6ac6f55a94666e2590a86d299f8e4aa0097a54
Mike Shvartsman <mshvarts@umich.edu>
2013-11-16 20:18:26 -0500

    typo fixes (how did this even run?) and also some more verbose output

--- data_munging/iso3-encoding.R
+++ data_munging/iso3-encoding.R
@@ -13,10 +13,15 @@ iso3_country <- countrycode(ai$country, "country.name", "iso3c") # Coding into I
 iso3_subject <- countrycode(ai$subject, "country.name", "iso3c") # Coding into ISO3. 
 iso3_category <- countrycode(ai$category, "country.name", "iso3c") # Coding into ISO3. 
 
-iso3 <- iso3_country
-iso3[is.na(ios3)] <- iso3_subject[is.na(iso3)]
-iso3[is.na(ios3)] <- iso3_category[is.na(iso3)]
+total <- length(iso3_country)
+
+iso3 <- iso3_country 
+print(sprintf("%s/%s are missing after using country field...",sum(is.na(iso3)), total))
+iso3[is.na(iso3)] <- iso3_subject[is.na(iso3)]
+print(sprintf("%s/%s are missing after using subject field...",sum(is.na(iso3)), total))
+iso3[is.na(iso3)] <- iso3_category[is.na(iso3)] 
+print(sprintf("%s/%s are missing after using category field...",sum(is.na(iso3)), total))
 
 ai2 <- cbind(ai, iso3) # Adding columns (the safe way). 
-
+print("...done!")
 write.csv(ai2, file="../cleaned_data/lotus_database_w_iso3.csv") # output.
